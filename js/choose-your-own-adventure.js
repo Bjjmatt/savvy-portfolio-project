428445a3a5e58885d64cd3ac328a85ac7115e283
Matthew Ricehouse <bjjmatt@yahoo.com>
2018-03-01 20:55:57 -0600

    Loops

--- js/choose-your-own-adventure.js
+++ js/choose-your-own-adventure.js
@@ -1,8 +1,10 @@
 var story = {
     "start": "You open your eyes to an alarm. Get up or snooze button?",
-    "snoozeButton": "You open your eyes to an alarm. Get up or snooze?",
-    "getUp": "As you wipe your eyes you can smell the coffee brewing. Go downstairs or skip breakfast?",
-    "goDownstairs": "Pancakes are waiting! Coffee black or with cream?",
+    "snooze": "You open your eyes to an alarm. Get up or snooze?",
+    "up": "As you wipe your eyes you can smell the coffee brewing. Go downstairs or skip breakfast?",
+    "downstairs": "Pancakes are waiting! Coffee black or with cream?",
+    "black": "Enjoy",
+    "cream": "Yuck",
     "skipBreakfast": "Getting dressed for the day your cell phone rings. Answer the phone or ignore it?",
     "answerPhone": "It's your mom yelling GET TO BREAKFAST!",
     "ignoreIt": "Your cell phone rings.",
@@ -10,20 +12,19 @@ var story = {
 };
 
 var choice = prompt( story.start );
+var output = document.querySelector( "#output" );
 
-if( choice === "snoozeButton" ){
-    choice = prompt( story.snoozeButton );
+
+if( choice === "snooze" ){
+    output.textContent = story.snooze;
 }
-else if( choice === "getUp" ){
-    choice = prompt( story.getUp );
+else if( choice === "up" ){
+    choice = prompt( story.up );
 
-    if( choice === "getUp" ){
-        console.log( story.getUp );
+    if( choice === "up" ){
+        output.textContent = story.up;
     }
     else{
-        console.log( "That's not an option yo!" );
-    }
-}
-else{
-    console.log( "That's not an option yo!" );
-}
+        output.textContent = "That's not an option yo!";
+
+  
